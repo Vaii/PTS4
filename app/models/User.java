731f3c4787c191a5548c1f98bb910507b2a7686d
MeijerM1 <max1_meijer@hotmail.com>
2017-09-13 14:31:25 +0200

    Added jackson dependency, added User class
    - Added jongo dependency jackson
    - Added user class
    - Added Role enum

---
+++ app/models/User.java
@@ -0,0 +1,70 @@
+package models;
+
+import org.jongo.marshall.jackson.oid.MongoObjectId;
+
+public class User {
+
+    @MongoObjectId
+    private int _id;
+    private String FirstName,LastName;
+    private String Email;
+    private Role role;
+    private String company;
+
+    public User(int _id, String firstName, String lastName, String email, Role role, String company) {
+        this._id = _id;
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+        this.role = role;
+        this.company = company;
+    }
+
+    public int get_id() {
+        return _id;
+    }
+
+    public void set_id(int _id) {
+        this._id = _id;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+}
--- build.sbt
+++ build.sbt
@@ -11,6 +11,9 @@ libraryDependencies ++= Seq( javaJdbc ,  cache , javaWs )
 // https://mvnrepository.com/artifact/org.mongodb/mongo-java-driver
 libraryDependencies += "org.mongodb" % "mongo-java-driver" % "3.5.0"
 
+// https://mvnrepository.com/artifact/org.jongo/jongo
+libraryDependencies += "org.jongo" % "jongo" % "1.3.0"
+
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )  
 
 resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"  
