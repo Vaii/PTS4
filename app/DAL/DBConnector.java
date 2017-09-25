6b4cab24b127b54432a78054a41a49fce12fe019
MeijerM1 <max1_meijer@hotmail.com>
2017-09-25 19:41:07 +0200

    Added connection class
    Connection class can now establish connection with mongo database

--- app/DAL/DBConnector.java
+++ app/DAL/DBConnector.java
@@ -1,5 +1,37 @@
 package DAL;
 
+import com.mongodb.DB;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+import org.jongo.Jongo;
+import org.jongo.MongoCollection;
+
 public class DBConnector {
-    
+    private DB database;
+    private Jongo jongo;
+
+    public DBConnector() {
+
+        // DB Password in code, We'll need a way around this.
+        MongoClientURI uri = new MongoClientURI(
+                "mongodb://InfoSupport:InfoSupport123@cluster0-shard-00-00-vx027.mongodb.net:27017,cluster0-shard-00-01-vx027.mongodb.net:27017,cluster0-shard-00-02-vx027.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin");
+
+        MongoClient mongoClient = new MongoClient(uri);
+
+        // Hardcoded database name
+        // TODO Make into variable
+        database = mongoClient.getDB("InfoSupport");
+
+        jongo = new Jongo((DB) database);
+    }
+
+    /**
+     *  If the collection does not exist a new one with the given name will be created.
+     * @param collecion The name of the collection.
+     * @return The given Collection.
+     */
+    public MongoCollection getCollection(String collecion) {
+        return jongo.getCollection(collecion);
+    }
 }
