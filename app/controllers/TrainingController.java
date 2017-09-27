3b63c6f5eb49da6b9204a64c89b06ef369be8a8f
Ken <k.odigwegomez@student.fontys.nl>
2017-09-27 15:41:18 +0200

    Trainingoverview added

--- app/controllers/TrainingController.java
+++ app/controllers/TrainingController.java
@@ -2,10 +2,14 @@ package controllers;
 
 import play.mvc.Controller;
 import play.mvc.Result;
-import views.html.*;
+import views.html.trainingoverview;
 
 /**
  * Created by Ken on 27-9-2017.
  */
 public class TrainingController extends Controller{
+
+    public Result overview() {
+        return ok(trainingoverview.render());
+    }
 }
