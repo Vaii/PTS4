364637730969f35dc39afeb682e429c9b8116d73
Kennyproz <k.odigwegomez@student.fontys.nl>
2017-09-27 19:37:48 +0200

    Changed Trainingoverview

--- app/controllers/TrainingController.java
+++ app/controllers/TrainingController.java
@@ -1,15 +1,41 @@
 package controllers;
 
+import models.Training;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.trainingoverview;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Ken on 27-9-2017.
  */
 public class TrainingController extends Controller{
 
+    //TrainingRepository trainingRepository = new TrainingRepository(context);
+
+  //  public final List<Training> trainingList;
+
+    @Inject
+    public TrainingController(){
+    /*    trainingList = new ArrayList<Training>();
+        trainingList.add(new Training("TestTraining"));
+        trainingList.add(new Training("TestTraining"));
+        trainingList.add(new Training("TestTraining"));
+        trainingList.add(new Training("TestTraining"));
+        trainingList.add(new Training("TestTraining"));
+*/
+    }
     public Result overview() {
         return ok(trainingoverview.render());
     }
+
+
+    //List<Training> training = new ArrayList<Training>();
+
+
+
+
 }
