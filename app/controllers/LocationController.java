ac43b236b42578b39964329e29d8ae18718ca371
Vaii <Vai@MacBook-Pro.local>
2017-10-02 13:36:43 +0200

    Revert "Merge branch 'Managing-Training-and-Location-Data-V3' into training-inschrijven-V2"
    This reverts commit e1e2a59d3ea16aef1b7fa4024bf9577cfa2a9fa0, reversing
    changes made to 6eee96eb90465a6b64d3070447da63e097b9b918.

--- app/controllers/LocationController.java
+++ app/controllers/LocationController.java
@@ -6,13 +6,11 @@ import models.Location;
 import play.data.Form;
 import play.data.FormFactory;
 import play.mvc.Result;
-import views.html.alllocations;
 
 import javax.inject.Inject;
 
 import java.util.ArrayList;
 
-import static play.mvc.Controller.flash;
 import static play.mvc.Results.badRequest;
 import static play.mvc.Results.ok;
 import static play.mvc.Results.redirect;
@@ -30,13 +28,9 @@ public class LocationController {
 
     //Loads the generic form for adding a location
     public Result loadLocationForm(){
-        return ok(views.html.addlocation.render(form));
+        return ok(views.html.newlocationform.render(form));
     }
 
-    //loads the page with available locations
-    public Result locationOverview(){
-        return ok(alllocations.render(locationrepo.getAll()));
-    }
 
     // method to get the results from the html form and redirect the user to the next page
     public Result createLocation(){
@@ -45,14 +39,12 @@ public class LocationController {
         if(boundForm.hasErrors()){
             play.Logger.ALogger logger = play.Logger.of(getClass());
             logger.error("errors ={}", boundForm.errors());
-            return badRequest(views.html.addlocation.render(boundForm));
+            return badRequest(views.html.newlocationform.render(boundForm));
         }
         else{
             Location data = boundForm.get();
-            Location newlocation = new Location(data.getCity(), data.getStreet(), data.getStreetNumber(), data.getRoom(), data.getCapacity());
-
-            if(locationrepo.addLocation(newlocation)){
-                return redirect(routes.LocationController.locationOverview());
+            if(locationrepo.addLocation(data)){
+                return redirect(routes.LocationController.loadLocationForm());
             }
             return redirect(routes.LocationController.loadLocationForm());
 
--- app/controllers/TrainingController.java
+++ app/controllers/TrainingController.java
@@ -1,32 +1,16 @@
 package controllers;
 
-import dal.contexts.TrainingMongoContext;
-import dal.interfaces.TrainingContext;
-import dal.repositories.TrainingRepository;
-import models.Training;
+import models.Secured;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
-
-import javax.inject.Inject;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Ken on 27-9-2017.
  */
 public class TrainingController extends Controller{
 
-    TrainingRepository trainingRepository = new TrainingRepository(new TrainingMongoContext("Training"));
-
-    public Result overview() {
-        return ok(trainingoverview.render(trainingRepository.getAll()));
-    }
-
-/*    public Result trainingClicked() {
-        return ok(training.render("Training"));
-    }*/
     public Result signUpCourse(){
-        return ok(signUpCourse.render());
+        return ok(signUpCourse.render("Training Inschrijven", Secured.isLoggedIn(ctx()), Secured.getUserInfo(ctx())));
     }
 }
