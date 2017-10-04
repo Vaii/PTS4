e71123dc89ebc1428668b24f42747a200ba333de
MeijerM1 <max1_meijer@hotmail.com>
2017-10-04 09:14:32 +0200

    Name changing

--- app/Main.java
+++ app/Main.java
@@ -1,10 +1,3 @@
-import dal.DBConnector;
-import dal.contexts.UserMongoContext;
-import dal.repositories.UserRepository;
-import models.Role;
-import models.User;
-import org.jongo.MongoCollection;
-
 public class Main {
 
     public static void main(String[] args) {
