204d2470600d69d347ce70e7042c392f727d915b
MeijerM1 <max1_meijer@hotmail.com>
2017-12-02 12:14:30 +0100

    Added JQuery Checks
    - Added check between password and validation
    - Added check for unique trainingcode

--- app/controllers/UtilityController.java
+++ app/controllers/UtilityController.java
@@ -1,6 +1,8 @@
 package controllers;
 
+import dal.contexts.TrainingMongoContext;
 import dal.contexts.UserMongoContext;
+import dal.repositories.TrainingRepository;
 import dal.repositories.UserRepository;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -8,12 +10,21 @@ import play.mvc.Result;
 public class UtilityController extends Controller {
 
     private UserRepository userRepo = new UserRepository(new UserMongoContext("User"));
+    private TrainingRepository trainingRepo = new TrainingRepository(new TrainingMongoContext("Training"));
 
     public Result checkEmail(String email) {
         if(userRepo.getUser(email) != null) {
             return ok("user_exists");
         } else {
             return ok("user_nonexists");
+        }
+    }
+
+    public Result checkTraningCode(String trainingCode) {
+        if(trainingRepo.getTraining(trainingCode) != null) {
+            return ok("code_exists");
+        } else {
+            return ok("code_nonexists");
         }
     }
 }
--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,17 +1,24 @@
 $( document ).ready(function () {
     $('#email').on('input', function() {
-        if($('#email').val() != "") {
+        if($('#email').val() !== "") {
             $.post('/utility/emailCheck/' + $('#email').val(), function(data) {
-                if(data== "user_exists"){
-                    console.log("User already exists");
+                if(data=== "user_exists"){
                     disableControls(true);
                 }else{
-                    console.log("User doesn't exists");
                     disableControls(false);
                 }
             });
         } else {
             disableControls(false);
+        }
+    });
+
+    $('#validation').on('input', function() {
+        var pass = $('#password').val();
+        var val = $('#validation').val();
+
+        if(pass !== "" && val !== "") {
+            checkPasswords(pass, val);
         }
     });
 
@@ -24,5 +31,17 @@ $( document ).ready(function () {
             $('#email_field').removeClass('has-danger');
             $('#validation_error_0').css("display", "none");
         }
+    }
+
+    function checkPasswords(pass, val) {
+            if(pass !== val) {
+                $('.btn-default').prop("disabled",true);
+                $('#validation_field').addClass('has-danger');
+                $('#validation_error_1').css("display", "block");
+            } else {
+                $('.btn-default').prop("disabled",false);
+                $('#validation_field').removeClass('has-danger');
+                $('#validation_error_1').css("display", "none");
+            }
     }
 });
